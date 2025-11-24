3347552599fe80c5d48a5a82bb2fd86c4afdcc59
Fonzy <fonzy.fermin@gmail.com>
2025-11-24 16:41:40 +0800

    Move sequential code to includes folder

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include "Sequential.hpp"
+#include <nbody_naive/Sequential.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
